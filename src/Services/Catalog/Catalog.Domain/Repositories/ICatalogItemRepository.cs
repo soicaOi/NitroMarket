c5e82761f44c4bcd30b198b03de6ec7caf00537f
soicaOi <ilnureva@gmail.com>
2025-10-13 21:41:07 +0400

    006 - Добавлен репозиториq ICatalogItemRepository.

---
+++ src/Services/Catalog/Catalog.Domain/Repositories/ICatalogItemRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Catalog.Domain.Repositories;
+
+public interface ICatalogItemRepository
+{
+    Task<CatalogItem> CreateCatalogItemAsync(CatalogItem item);
+    Task<IEnumerable<CatalogItem>> GetAllCatalogItemsAsync();
+    Task<CatalogItem?> GetCatalogItemAsync(Guid id);
+    Task<IEnumerable<CatalogItem>> GetCatalogItemsByTitleAsync(string title);
+    Task<IEnumerable<CatalogItem>> GetCatalogItemsByBrandAsync(string brandTitle);
+    Task<bool> UpdateCatalogItemAsync(CatalogItem item);
+    Task<bool> DeleteCatalogItemAsync(Guid id);
+}
