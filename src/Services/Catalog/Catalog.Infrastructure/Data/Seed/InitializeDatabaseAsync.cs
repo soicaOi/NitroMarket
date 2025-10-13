fe153fa87847f5efacc37e4cc1c58bd8f8e83f08
soicaOi <ilnureva@gmail.com>
2025-10-13 22:07:40 +0400

    010 - Добавлен класс для инициализации и наполнения БД тестовыми данными.

---
+++ src/Services/Catalog/Catalog.Infrastructure/Data/Seed/InitializeDatabaseAsync.cs
@@ -0,0 +1,35 @@
+﻿using Marten;
+using Marten.Schema;
+
+namespace Catalog.Infrastructure.Data.Seed;
+
+public class InitializeDatabaseAsync : IInitialData
+{
+    public async Task Populate(IDocumentStore store, CancellationToken cancellation)
+    {
+        using var session = store.LightweightSession();
+
+        if (!await session.Query<Brand>().AnyAsync())
+        {
+            session.Store<Brand>(InitialData.Brands);
+        }
+
+        foreach (var category in InitialData.Categories)
+        {
+            if (session.Query<Category>().Any(c => c.Id == category.Id))
+            {
+                session.Store(category);
+            }
+        }
+
+        foreach (var item in InitialData.CatalogItems)
+        {
+            if (session.Query<Category>().Any(c => c.Id == item.Id))
+            {
+                session.Store(item);
+            }
+        }
+
+        await session.SaveChangesAsync();
+    }
+}
