1ebb10d0fa5ead14e577dc9edfa2d17ef288ee7e
soicaOi <ilnureva@gmail.com>
2025-10-19 18:20:33 +0400

    015 - Добавлен контроллер (BrandsController) для получения брендов через MediatR.

---
+++ src/Services/Catalog/Catalog.API/Controllers/BrandsController.cs
@@ -0,0 +1,18 @@
+﻿using Catalog.Application.Queries.BrandQueries;
+using Catalog.Application.Responses.BrandResponses;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BrandsController(IMediator mediator) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<GetBrandsResult>> GetBrand()
+    {
+        var result = await mediator.Send(new GetBrandsQuery());
+        return result;
+    } 
+}
